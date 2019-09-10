436071f920e11dda7d479de3b0b0b4fe5186361f
lclarete <livia.clarete@gmail.com>
2019-09-10 16:52:04 -0300

    updates

--- week_2/16_lab_mysql_week2/your-code/seeding.sql
+++ week_2/16_lab_mysql_week2/your-code/seeding.sql
@@ -1,13 +1,13 @@
-insert into car () values ("3K096I98581DHSNUP",	"Volkswagen",	"Tiguan",	2019,	"Blue");
-insert into car () values ("ZM8G7BEUQZ97IH46V",	"Peugeot",	"Rifter",	2019,	"Red");
-insert into car () values ("RKXVNNIHLVVZOUB4M",	"Ford",	"Fusion",	2018,	"White");
-insert into car () values ("HKNDGS7CU31E9Z7JW",	"Toyota",	"RAV4",	2018,	"Silver");
-insert into car () values ("DAM41UDN3CHU2WVF6",	"Volvo",	"V60",	2019,	"Gray");
-insert into car () values ("DAM41UDN3CHU2WVF6",	"Volvo",	"V60 Cross Country",	2019,	"Gray");
+insert into car () values ("0","3K096I98581DHSNUP",	"Volkswagen",	"Tiguan",	2019,	"Blue");
+insert into car () values ("1","ZM8G7BEUQZ97IH46V",	"Peugeot",	"Rifter",	2019,	"Red");
+insert into car () values ("2","RKXVNNIHLVVZOUB4M",	"Ford",	"Fusion",	2018,	"White");
+insert into car () values ("3","HKNDGS7CU31E9Z7JW",	"Toyota",	"RAV4",	2018,	"Silver");
+insert into car () values ("4","DAM41UDN3CHU2WVF6",	"Volvo",	"V60",	2019,	"Gray");
+insert into car () values ("5","DAM41UDN3CHU2WVF6",	"Volvo",	"V60 Cross Country",	2019,	"Gray");
 
-insert into customer () values (10001,	"Pablo Picasso",	"+34 636 17 63 82",		 "-",  	"Paseo de la Chopera 14",	"Madrid",	"Madrid",	"Spain",	28045);
-insert into customer () values (20001,	"Abraham Lincoln",	"+1 305 907 7086", 		 "-", 	"120 SW 8th St",			"Miami",	"Florida",	"United States",	33130);
-insert into customer () values (30001,	"Napoléon Bonaparte",	"+33 1 79 75 40 00", "-", 	"40 Rue du Colisée",		"Paris",	"Île-de-France", 	"France",	75008);
+insert into customer () values (10001,"Pablo Picasso","+34 636 17 63 82","-","Paseo de la Chopera 14","Madrid",	"Madrid","Spain",28045);
+insert into customer () values (20001,"Abraham Lincoln","+1 305 907 7086", "-","120 SW 8th St","Miami",	"Florida","United States",33130);
+insert into customer () values (30001,"Napoléon Bonaparte",	"+33 1 79 75 40 00","-","40 Rue du Colisée","Paris","Île-de-France", "France",75008);
 
 insert into salesperson () values (00001,	"Petey Cruiser",	"Madrid");
 insert into salesperson () values (00002,	"Anna Sthesia",	"Barcelona");
--- week_2/16_lab_mysql_week2/your-code/tables.sql
+++ week_2/16_lab_mysql_week2/your-code/tables.sql
@@ -1,16 +1,19 @@
 create table customer (customerID int, 
 					customerName varchar(50), 
-                    customerPhone int,
+                    customerPhone varchar(50),
                     customerEmail varchar(50),
                     customerAddress varchar(50),
                     customerCity varchar(50),
                     customerState varchar(50),
+                    customerCountry varchar(50),
                     customerZip int);
                     
-create table car (VIN varchar(50), 
+create table car (carID int,
+				VIN varchar(50), 
 				manufacturer varchar(50),
                 model varchar(50),
-                year int);
+                year int,
+                color varchar(50));
 
 create table salesperson (staffID int,
 						staffName varchar(50),
